67c5d2c1640f6e4d8a07e9666d31477730dbc4b8
anrpro <anrpro@gmail.com>
2018-08-16 22:10:07 +0530

    file upload

--- WebDemo/Controllers/CustomerController.cs
+++ WebDemo/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebDemo.Models;
 
 namespace WebDemo.Controllers
 {
@@ -13,5 +14,47 @@ namespace WebDemo.Controllers
         {
             return View();
         }
+
+        public static CustomerInfo customerDet = new CustomerInfo
+                                                {
+                                                    Id = 101,
+                                                    Name = "Anr",
+                                                    Email = "anrpro@gmail.com",
+                                                    attachments = new List<string>()
+
+            
+                                    };
+
+
+        public ActionResult UpdateCustomer()
+        {
+            return View(customerDet);
+
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdateCustomer(CustomerInfo customer, List<HttpPostedFileBase> files)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(customer);
+            }
+
+            customerDet.Name = customer.Name;
+            customerDet.Email = customer.Email;
+
+            foreach (HttpPostedFileBase file in files)
+            {
+                
+                file.SaveAs(System.IO.Path.Combine(Server.MapPath("~/Documents/"), file.FileName));
+                customerDet.attachments.Add(file.FileName);
+
+            }
+
+            return View(customerDet);
+
+        }
+
     }
 }
---
+++ WebDemo/Models/CustomerInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDemo.Models
+{
+    public class CustomerInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<string> attachments { get; set; }
+    }
+}
